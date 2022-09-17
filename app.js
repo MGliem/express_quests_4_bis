a48eeeac71403068020b083dd3f36c3fe9d5a08e
Gliem <maximilian.gliem@gmail.com>
2022-09-17 20:46:51 +0200

    challenge express 4 bis: validations

--- app.js
+++ app.js
@@ -14,14 +14,14 @@ app.get("/", welcome);
 const movieHandlers = require("./movieHandlers");
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
-app.post("/api/movies", movieHandlers.postMovie);
-app.put("/api/movies/:id", movieHandlers.updateMovie);
+app.post("/api/movies", movieHandlers.validateMovie, movieHandlers.postMovie);
+app.put("/api/movies/:id", movieHandlers.validateMovie, movieHandlers.updateMovie);
 
 const userHandlers = require('./userHandlers');
 app.get('/api/users', userHandlers.getUsers);
 app.get('/api/users/:id', userHandlers.getUserById);
-app.post("/api/users", userHandlers.postUser);
-app.put("/api/users/:id", userHandlers.updateUser);
+app.post("/api/users", userHandlers.validateUser, userHandlers.validateUniqueEmail, userHandlers.postUser);
+app.put("/api/users/:id", userHandlers.validateUser, userHandlers.validateUniqueEmail, userHandlers.updateUser);
 
 app.listen(port, (err) => {
   if (err) {
--- userHandlers.js
+++ userHandlers.js
@@ -1,33 +1,75 @@
 const database = require("./database");
+const joi = require("joi");
 
-const getUsers = (req, res) => {
-    database
-    .query('select * from users')
-    .then(([users]) => {
-        res.json(users);
+const userSchema = joi.object({
+  firstname: joi.string().max(255).required(),
+  lastname: joi.string().max(255).required(),
+  email: joi.string().email().required(),
+  city: joi.string().max(255),
+  language: joi.string().max(255),
+});
+
+const validateUser = (req, res, next) => {
+  const { firstname, lastname, email, city, language } = req.body;
+
+  const { error } = userSchema.validate(
+    { firstname, lastname, email, city, language },
+    { abortEarly: false }
+  );
+
+  if (error) {
+    res.status(422).json({ validationErrors: error.details });
+  } else {
+    next();
+  }
+};
+
+const validateUniqueEmail = (req, res, next) => {
+  const { email } = req.body
+  database
+    .query('select email from users where email = ?', [email])
+    .then(([email]) => {
+      if (email.length !== 0) {
+        console.log(email);
+        res.status(422).send('Email must be unique');
+      } else {
+        next();
+      }
     })
     .catch((err) => {
-        console.error(err);
-        res.status(500).send("Error retrieving data from database");
+      console.error(err);
+      res.status(500).send('Database error');
+    });
+};
+
+const getUsers = (req, res) => {
+  database
+    .query("select * from users")
+    .then(([users]) => {
+      res.json(users);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error retrieving data from database");
     });
 };
 
 const getUserById = (req, res) => {
-    const id = parseInt(req.params.id);
+  const id = parseInt(req.params.id);
 
-    database
-      .query('select * from users where id = ?', [id])
-      .then(([users]) => {
-        if (users[0] != null) {
-          res.json(users[0]);
-        } else {
-          res.status(404).send("Not Found");
-        }
-      })
-      .catch((err) => {
-        console.error(err);
-        res.status(500).send("Error retrieving data from database");
-      });
+  database
+    .query("select * from users where id = ?", [id])
+    .then(([users]) => {
+      if (users[0] != null) {
+        res.json(users[0]);
+      } else {
+        res.status(404).send("Not Found");
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error retrieving data from database");
+    });
 };
 
 const postUser = (req, res) => {
@@ -35,7 +77,7 @@ const postUser = (req, res) => {
 
   database
     .query(
-      'insert into users (firstname, lastname, email, city, language) values (?, ?, ?, ?, ?)', 
+      "insert into users (firstname, lastname, email, city, language) values (?, ?, ?, ?, ?)",
       [firstname, lastname, email, city, language]
     )
     .then(([result]) => {
@@ -45,32 +87,35 @@ const postUser = (req, res) => {
       console.error(err);
       res.status(500).send("Error saving the user");
     });
-}
+};
 
 const updateUser = (req, res) => {
   const id = parseInt(req.params.id);
-  const { firstname, lastname, email, city, language } = req.body
+  const { firstname, lastname, email, city, language } = req.body;
 
   database
     .query(
-      'update users set firstname = ?, lastname = ?, email = ?, city = ?, language = ?',
-      [firstname, lastname, email, city, language])
+      "update users set firstname = ?, lastname = ?, email = ?, city = ?, language = ?",
+      [firstname, lastname, email, city, language]
+    )
     .then(([result]) => {
       if (result.affectedRows === 0) {
-        res.status(404).send('Not Found');
+        res.status(404).send("Not Found");
       } else {
         res.sendStatus(204);
       }
     })
     .catch((err) => {
       console.error(err);
-      res.status(500).send('Error editing the user')
-    })
-}
+      res.status(500).send("Error editing the user");
+    });
+};
 
 module.exports = {
-    getUsers,
-    getUserById,
-    postUser,
-    updateUser
-}
+  getUsers,
+  getUserById,
+  postUser,
+  updateUser,
+  validateUser,
+  validateUniqueEmail
+};
