ee09591c2cd284cbcae5a94bd4a6a69ee171ba6a
Gliem <maximilian.gliem@gmail.com>
2022-09-17 16:45:13 +0200

    challenge quest 3

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ app.post("/api/movies", movieHandlers.postMovie);
 const userHandlers = require('./userHandlers');
 app.get('/api/users', userHandlers.getUsers);
 app.get('/api/users/:id', userHandlers.getUserById);
+app.post("/api/users", userHandlers.postUser);
 
 app.listen(port, (err) => {
   if (err) {
--- userHandlers.js
+++ userHandlers.js
@@ -16,21 +16,40 @@ const getUserById = (req, res) => {
     const id = parseInt(req.params.id);
 
     database
-    .query('select * from users where id = ?', [id])
-    .then(([users]) => {
-      if (users[0] != null) {
-        res.json(users[0]);
-      } else {
-        res.status(404).send("Not Found");
-      }
+      .query('select * from users where id = ?', [id])
+      .then(([users]) => {
+        if (users[0] != null) {
+          res.json(users[0]);
+        } else {
+          res.status(404).send("Not Found");
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send("Error retrieving data from database");
+      });
+};
+
+const postUser = (req, res) => {
+  const { firstname, lastname, email, city, language } = req.body;
+
+  database
+    .query(
+      'insert into users (firstname, lastname, email, city, language) values (?, ?, ?, ?, ?)', 
+      [firstname, lastname, email, city, language]
+    )
+    .then(([result]) => {
+      res.location(`/api/users/${result.insertId}`).sendStatus(201);
     })
     .catch((err) => {
       console.error(err);
-      res.status(500).send("Error retrieving data from database");
+      res.status(500).send("Error saving the user");
     });
-};
+
+}
 
 module.exports = {
     getUsers,
-    getUserById
+    getUserById,
+    postUser,
 }
